de2117fb92239c70d16ee3a16d24a7f848bc2ed1
MartinD-CZ <martin@embedblog.eu>
2021-04-17 16:31:37 +0200

    minor fixes

---
+++ Inc/libCONF.h
@@ -0,0 +1,7 @@
+#ifndef LIBCONF_H_
+#define LIBCONF_H_
+
+#define F_SYSCLK				16000000UL
+#define SALLOC_SIZE				512
+
+#endif /* LIBCONF_H_ */
--- Src/main.cpp
+++ Src/main.cpp
@@ -1,11 +1,20 @@
-#include <stdint.h>
+#include "libCORE.h"
+#include "libGPIO.h"
+#include "libUART.h"
 
+gpio rled{GPIOA, 7};
 
 int main(void)
 {
+	rcc::initHSI();
+	rcc::initSystemClock(rcc::vRange::high, rcc::sysclk::HSI);
+	rcc::disableMSI();
+
+	rled.initOutput(gpio::speed::low, gpio::output::high, gpio::type::opendrain);
 
 	while (1)
 	{
-
+		tick::delay(500);
+		rled.toggle();
 	}
 }
