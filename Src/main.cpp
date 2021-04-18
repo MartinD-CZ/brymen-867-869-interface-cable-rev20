81756394d3d202c5a63ea2a2b0f448dd932b1360
MartinD-CZ <martin@embedblog.eu>
2021-04-18 16:08:52 +0200

    tested EEPROM, added version.h into gitignore

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,6 +1,7 @@
 #include "libCORE.h"
 #include "libGPIO.h"
 #include "libUART.h"
+#include "libNVM.h"
 
 #include "version.h"
 
@@ -10,7 +11,7 @@ gpio phototransistor{GPIOA, 0};
 
 uart_irq com{USART2};
 
-char com_txBuffer[64];
+char com_txBuffer[128];
 char com_rxBuffer[4];
 
 int main(void)
@@ -31,13 +32,22 @@ int main(void)
 
 	//print initial info on the serial port
 	com.print("\n\nBrymen 867/869 interface cable\nfor more info, see embedblog.eu/?p=475\n\n");		//TODO:web
-	com.printf("Firmware revision: %s", _V_BUILD_TAG);
+	com.printf("Firmware revision: %s\n", _V_BUILD_TAG);
 	com.print("Available commands:\n");
 	com.print("F - 5 samples per second\n");
 	com.print("O - 1 sample per second\n");
 	com.print("S - stop autosend\n");
 	com.print("D - send a single reading\n");
 	com.print("R - toggle raw data output\n");
+
+	/*eeprom::enableClock();
+	uint64_t test = 0xDEADBEEFABCDEFC4;
+	eeprom::write<uint64_t>(0, test);
+
+	uint64_t readback;
+	eeprom::read<uint64_t>(0, &readback);
+
+	com.printf("Data readback: 0x%x\n", readback);*/
 
 	while (1)
 	{
